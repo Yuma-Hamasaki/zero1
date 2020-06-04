395cdd717816b21c979887ab922172e18a94dad3
Yoshiki Imatake <y-imatake@cresco.co.jp>
2020-06-04 20:07:10 +0900

    新規追加

--- 04_implementation/source/ReservationList.java
+++ 04_implementation/source/ReservationList.java
@@ -1,31 +1,59 @@
 package チケット予約システム;
 
-public class ReservationList {
+import java.util.ArrayList;
+import java.util.List;
 
-	private Reservation[] reservation;
+public class ReservationList {
+	//ReservationListクラスが返す配列の戻り値は、すべてList型にする。
+	private List<Reservation> reservationList = new ArrayList<Reservation>();
 
 	public ReservationList() {
 
 	}
 
 	public void addReservation(Reservation reservation) {
-
+		reservationList.add(reservation);
 	}
-
+	/*
 	public Reservation getReservation(int reservationNo) {
+		
 		return null;
 	}
-
+	*/
 	public int getAmount(int reservationNo) {
+		for(Reservation rsv : reservationList) {
+			int currentNo = rsv.getReservationNo();
+			if(reservationNo == currentNo) {
+				return rsv.getAmount();
+			}
+		}
 		return 0;
 	}
 
 	public void deleteReservation(int reservationNo) {
+		for(int i=0; i<reservationList.size(); i++) {
+			Reservation rsv = reservationList[i]
+			int currentNo = rsv.getReservationNo();
+			if(reservationNo == currentNo) {
+				reservationList.remove(i);
+			}
+		}
 
 	}
 
-	public Reservation[] getAllReservation(String memberID) {
-		return null;
+	public List<Reservation> getAllReservation(String memberID) {
+		List<Reservation> returnList = new ArrayList<Reservation>();
+		for(int i=0; i<reservationList.size(); i++) {
+			Reservation rsv = reservationList[i]
+			if(rsv.checkMatchID(memberID)) {
+				returnList.add(rsv);
+			}
+		}
+		if(returnList.size()==0) {
+			return null;
+		}else {
+			return returnList;
+		}
 	}
 
 }
