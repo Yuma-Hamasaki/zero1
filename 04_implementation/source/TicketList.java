47ddee6ed4334847dbdc0aea8f96a95597c9af8b
Yuma-Hamasaki <y-hamasaki@cresco.co.jp>
2020-06-04 22:23:31 +0900

    ticketListクラスの記述

--- 04_implementation/source/TicketList.java
+++ 04_implementation/source/TicketList.java
@@ -1,9 +1,49 @@
 package チケット予約システム;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class TicketList {
 
+	private List<Ticket> ticketList = new ArrayList<Ticket>();
+	
 	public TicketList() {
 
+	}
+	
+	public void addTicket(Ticket ticket) {
+		ticketList.add(ticket);
+	}
+	
+	public Ticket getTicket(int ticketNo, int reservationAmount) {
+		for(Ticket ticket: ticketList) {
+			if(ticketNo == ticket.getticketNo()) {
+				return ticket;
+			}
+		}
+		return null;
+	}
+	
+	public void addTicketStock(int ticketNo, int cancelAmount) {
+		for(Ticket ticket: ticketList) {
+			if(ticketNo == ticket.getticketNo()) {
+				ticket.addStock(cancelAmount);
+				return;
+			}
+		}
+	}
+	
+	public void reduceTicketStock(int ticketNo, int reservationAmount) {
+		for(Ticket ticket: ticketList) {
+			if(ticketNo == ticket.getticketNo()) {
+				ticket.reduceStock(reservationAmount);
+				return;
+			}
+		}
+	}
+	
+	public List<Ticket> getAllTicket() {
+		return ticketList;
 	}
 
 }
