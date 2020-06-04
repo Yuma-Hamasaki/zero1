3beee21841998b3c27c115846efadeebddee6428
Yuma-Hamasaki <y-hamasaki@cresco.co.jp>
2020-06-04 17:35:51 +0900

    スースコード自動生成しましたよぉ

---
+++ 04_implementation/source/CUI.java
@@ -0,0 +1,49 @@
+package 設計;
+
+public class CUI {
+
+	public CUI() {
+
+	}
+
+	public String inputPW() {
+		return null;
+	}
+
+	public String inputID() {
+		return null;
+	}
+
+	public SystemFunction selectFunction() {
+		return null;
+	}
+
+	public int inputReservationNo() {
+		return 0;
+	}
+
+	public int inputTicketNo() {
+		return 0;
+	}
+
+	public int inputTicketAmount() {
+		return 0;
+	}
+
+	public boolean confirm() {
+		return false;
+	}
+
+	public void display(Reservation[] displayedList) {
+
+	}
+
+	public void display(Ticket[] displayedList) {
+
+	}
+
+	public void showMessage(String message) {
+
+	}
+
+}
---
+++ 04_implementation/source/Member.java
@@ -0,0 +1,27 @@
+package 設計;
+
+public class Member {
+
+	private String id;
+
+	private String password;
+
+	private String name;
+
+	public Member(String memberID, String password, String name) {
+
+	}
+
+	public String getId() {
+		return null;
+	}
+
+	public String getPassword() {
+		return null;
+	}
+
+	public String getName() {
+		return null;
+	}
+
+}
